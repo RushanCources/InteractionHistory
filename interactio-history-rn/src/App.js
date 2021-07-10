168cd4b744c8b937505c03508a3d8fd788649e0e
ruro0915 <rushan.romazanov@NetCracker.com>
2021-07-10 14:45:54 +0300

    Initial commit

---
+++ interactio-history-rn/src/App.js
@@ -0,0 +1,21 @@
+import type {Node} from 'react';
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text,} from 'react-native';
+
+const App: () => Node = () => {
+    return (
+        <SafeAreaView>
+            <Text style={styles.sectionTitle}>Hello world!!!</Text>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+        backgroundColor: '#FFF',
+    },
+});
+
+export default App;
