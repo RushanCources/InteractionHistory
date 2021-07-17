86d9df22e705add0689172aa84623b9d7fae565e
GeorgiyCox <georgiycox@yandex.ru>
2021-07-17 18:21:33 +0300

    styling the navigation header of the contactВetailsTab

--- interactio-history-rn/src/navigation/NavigationStack/NavigationStack.tsx
+++ interactio-history-rn/src/navigation/NavigationStack/NavigationStack.tsx
@@ -3,26 +3,31 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import ContactsScreen from '../../components/ContactsScreen/ContactsScreen';
 import ContactInformationScreen from '../ContactInformationScreen/ContactInformationScreen';
+import ContactInformationTitleNavigation from '../ContactDetailsTitleNav/ContactDetailsTitleNav';
+import stylesMain from '../../styles.global'
 
 const Stack = createStackNavigator();
 
 const NavigationStack = () => {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen
-					name="ContactsScreen"
-					component={ContactsScreen}
-					options={{ title: 'Contacts' }}
-				/>
-				<Stack.Screen
-					name="ContactInformationScreen"
-					component={ContactInformationScreen}
-					options={{ title: 'Contact Information' }}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="ContactsScreen"
+          component={ContactsScreen}
+          options={{ title: 'Contacts' }}
+        />
+        <Stack.Screen
+          name="ContactInformationScreen"
+          component={ContactInformationScreen}
+					options={{
+						headerStyle: stylesMain.stackNavHeaderBigDarkBlue,
+						headerTitle: props => <ContactInformationTitleNavigation />,
+					}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default NavigationStack;
