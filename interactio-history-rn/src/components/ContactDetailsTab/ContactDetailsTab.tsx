56a0d5907a885e776c8adfad718ca6ef05045e0c
GeorgiyCox <georgiycox@yandex.ru>
2021-07-19 11:14:12 +0300

    fix fixed the display of elements in contactDetails

--- interactio-history-rn/src/components/ContactDetailsTab/ContactDetailsTab.tsx
+++ interactio-history-rn/src/components/ContactDetailsTab/ContactDetailsTab.tsx
@@ -6,21 +6,36 @@ import stylesMain from '../../styles.global';
 
 const ContactDetailsTab = () => {
   const { dataContact } = useContext(dataContactContext)
-  
+
   return (
     <SafeAreaView style={stylesMain.containerWhite}>
-      <ContactDetailsItem
-        communicationMethod='Mobile'
-        telOrNumer={dataContact.mobile}
-      />
-      <ContactDetailsItem 
-        communicationMethod='Work number'
-        telOrNumer={dataContact.work}
-      />
-      <ContactDetailsItem 
-        communicationMethod='Skype'
-        telOrNumer={dataContact.skype}
-      />
+      {dataContact.mobile &&
+        <ContactDetailsItem
+          communicationMethod='Mobile'
+          telOrNumer={dataContact.mobile}
+        />
+      }
+      {
+        dataContact.email && 
+        <ContactDetailsItem
+          communicationMethod='Mail'
+          telOrNumer={dataContact.email}
+        />
+      }
+      {
+        dataContact.work && 
+        <ContactDetailsItem
+          communicationMethod='Work number'
+          telOrNumer={dataContact.work}
+        />
+      }
+      {
+        dataContact.work && 
+        <ContactDetailsItem
+          communicationMethod='Skype'
+          telOrNumer={dataContact.skype}
+        />
+      }
     </SafeAreaView>
   );
 };
