4398c3d2bd67ed0b0333277f2e0c3251e73cf448
SherstnevVasiliy <root@kola-nn.com>
2021-07-21 23:53:56 +0300

    add class http and render records

---
+++ interactio-history-rn/src/common/config.ts
@@ -0,0 +1,4 @@
+const CONFIG = {
+    BASE_URL: 'https://interaction-history.herokuapp.com/'
+}
+export default CONFIG
--- interactio-history-rn/src/components/ContactInteractionsTab/ContactInteractionsTab.tsx
+++ interactio-history-rn/src/components/ContactInteractionsTab/ContactInteractionsTab.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {FlatList, SafeAreaView, Text, View, StyleSheet} from 'react-native';
+import {HTTP} from '../../common/http';
 
-const ContactInteractionsTab = () => {  
+const ContactInteractionsTab = () => {
+  const [interactions, setInteractions] = useState([]);
+
+  const renederItem = useCallback(
+    item => {
+      return (
+          <View style={styles.viewBorder}>
+            <Text>{`data: ${item.item.date}`}</Text>
+            <Text>{`descriptions: ${item.item.description}`}</Text>
+            <Text>{`contact_id: ${item.item.contact_id}`}</Text>
+            <View>
+              <Text>{`type_id: ${item.item.type.id}`}</Text>
+              <Text>{`type_name: ${item.item.type.name}`}</Text>
+            </View>
+          </View>
+      );
+    },
+    [],
+  );
+
+  useEffect(() => {
+    const getItem = async () => {
+      try {
+        const response = await HTTP.get(`records`);
+
+        if (response) {
+          setInteractions(response.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getItem();
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text> Welcome to Contact Interactions</Text>
+      <FlatList data={interactions} renderItem={renederItem} />
     </SafeAreaView>
   );
 };
 
-export default ContactInteractionsTab;
+const styles = StyleSheet.create({
+  viewBorder: {
+    borderBottomWidth: 1,
+  }
+})
+
+export default React.memo(ContactInteractionsTab);
