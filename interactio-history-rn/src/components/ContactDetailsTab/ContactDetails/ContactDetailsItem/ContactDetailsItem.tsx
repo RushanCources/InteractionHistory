2f779cb9ef08391d9fcc761fadab55d41d16d7c7
GeorgiyCox <georgiycox@yandex.ru>
2021-08-03 14:16:28 +0300

    fix ContactDetailsItem

--- interactio-history-rn/src/components/ContactDetailsTab/ContactDetails/ContactDetailsItem/ContactDetailsItem.tsx
+++ interactio-history-rn/src/components/ContactDetailsTab/ContactDetails/ContactDetailsItem/ContactDetailsItem.tsx
@@ -44,7 +44,6 @@ const ContactDetailsItem = ({ communicationMethod, telOrNumer }: IContactDetails
       onPress={() => {
         call(connectURL)
         navigation.navigate('CreateFormScreen')
-
       }}
     >
       <View style={[stylesMain.itemContainerBorderDown, stylesMain.containerRow]}>
